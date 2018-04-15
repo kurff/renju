8a23ad5bad2716b7f4091c558f0dd07c1de6f3bf
kurffu@gmail.com <kurffu@gmail.com>
2018-04-15 22:55:08 +0800

    update_net and predict_net

--- network/network.hpp
+++ network/network.hpp
@@ -10,7 +10,7 @@ namespace Beta{
     class Network{
         public:
             Network(int board_size, int batch_size, int channels): 
-            board_size_(board_size), batch_size_(batch_size), channels_(channels){
+            board_size_(board_size), batch_size_(batch_size), channels_(channels), output_dim_(board_size*board_size){
                 
 
             }
@@ -21,15 +21,18 @@ namespace Beta{
 
         public:
             
-            void init(NetDef& init_model, NetDef& predict_model, bool use_gpu){
+            void init(NetDef& init_model, NetDef& predict_model, NetDef& update_model, bool use_gpu){
                 
                 init_model_ = init_model;
                 predict_model_ = predict_model;
+                update_model_ = update_model;
                 init_net_.reset(new Net(init_model_));
-                predict_net_.reset(new Net(predict_model_)); 
+                predict_net_.reset(new Net(predict_model_));
+                update_net_.reset(new Net(update_model_)); 
                 if(use_gpu){
                     init_net_->SetDeviceCUDA();
                     predict_net_->SetDeviceCUDA();
+                    update_net_->SetDeviceCUDA();
                 }
                 workspace_.reset(new Workspace("workspace"));
 
@@ -37,71 +40,117 @@ namespace Beta{
                 Blob* data = workspace_->CreateBlob("data");
                 T* input = data->GetMutable<T>();
                 input->Resize(vector<int>{batch_size_,channels_,board_size_,board_size_});
-                input->mutable_data<float>();
+                input->template mutable_data<float>();
 
                 // create label pai, action prediction
                 Blob* pai = workspace_->CreateBlob("pai");
                 T* label_pai = pai->GetMutable<T>();
-                label_pai->Resize(vector<int>{batch_size_, 1, board_size_, board_size_});
-                label_pai->mutable_data<float>();
+                label_pai->Resize(vector<int>{batch_size_, board_size_* board_size_});
+                label_pai->template mutable_data<float>();
 
                 // create label z, game winner
                 Blob* z = workspace_->CreateBlob("z");
                 T* label_z = z->GetMutable<T>();
                 label_z->Resize(vector<int>{batch_size_, 1});
-
-                
-
+                label_z->template mutable_data<float>();
 
             }
+
+
 
 
             void allocate(){
                 init_ = CreateNet( init_model_,workspace_.get());
-                LOG(INFO)<<"create network start";
+                LOG(INFO)<<"create init network finish";
                 predict_ = CreateNet(predict_model_, workspace_.get());
-                LOG(INFO)<<"create network finish";
+                LOG(INFO)<<"create predict network finish";
+                update_ = CreateNet(update_model_, workspace_.get());
+                LOG(INFO)<<"create update network finish";
             }
 
 
 
+            void create_network(bool training){
+                create_lenet(training);
+
+
+            }
+
 
 
             void create_lenet(bool training){
+                update_net_->AddInput("data");
                 predict_net_->AddInput("data");
+
+                update_net_->AddInput("pai");
+
+                update_net_->AddConvOp("data", "conv1_w", "conv1_b", "conv1", 1, 0, 5);
                 predict_net_->AddConvOp("data", "conv1_w", "conv1_b", "conv1", 1, 0, 5);
+                
+                update_net_->AddInput("conv1_w");
                 predict_net_->AddInput("conv1_w");
+               
+                update_net_->AddInput("conv1_b");
                 predict_net_->AddInput("conv1_b");
+
                 if (training) {
                     init_net_->AddXavierFillOp({20, 1, 5, 5}, "conv1_w");
                     init_net_->AddConstantFillOp({20}, "conv1_b");
                 }
-                predict_net_->AddMaxPoolOp("conv1", "pool1", 2, 0, 2);
 
+                update_net_->AddMaxPoolOp("conv1", "pool1", 2, 0, 2);
+                update_net_->AddConvOp("pool1", "conv2_w", "conv2_b", "conv2", 1, 0, 5);
+                update_net_->AddInput("conv2_w");
+                update_net_->AddInput("conv2_b");
+
+                predict_net_->AddMaxPoolOp("conv1", "pool1", 2, 0, 2);
                 predict_net_->AddConvOp("pool1", "conv2_w", "conv2_b", "conv2", 1, 0, 5);
                 predict_net_->AddInput("conv2_w");
                 predict_net_->AddInput("conv2_b");
+
                 if (training) {
                     init_net_->AddXavierFillOp({50, 20, 5, 5}, "conv2_w");
                     init_net_->AddConstantFillOp({50}, "conv2_b");
                 }
+
+
+
+                update_net_->AddMaxPoolOp("conv2", "pool2", 2, 0, 2);
+                update_net_->AddFcOp("pool2", "fc3_w", "fc3_b", "fc3");
+                update_net_->AddInput("fc3_w");
+                update_net_->AddInput("fc3_b");
+
                 predict_net_->AddMaxPoolOp("conv2", "pool2", 2, 0, 2);
                 predict_net_->AddFcOp("pool2", "fc3_w", "fc3_b", "fc3");
                 predict_net_->AddInput("fc3_w");
                 predict_net_->AddInput("fc3_b");
+
+
                 if (training) {
                     init_net_->AddXavierFillOp({500, 800}, "fc3_w");
                     init_net_->AddConstantFillOp({500}, "fc3_b");
                 }
+                update_net_->AddReluOp("fc3", "fc3");
+                update_net_->AddFcOp("fc3", "pred_w", "pred_b", "pred");
+                update_net_->AddInput("pred_w");
+                update_net_->AddInput("pred_b");
+
                 predict_net_->AddReluOp("fc3", "fc3");
                 predict_net_->AddFcOp("fc3", "pred_w", "pred_b", "pred");
                 predict_net_->AddInput("pred_w");
                 predict_net_->AddInput("pred_b");
+
                 if (training) {
-                    init_net_->AddXavierFillOp({10, 500}, "pred_w");
-                    init_net_->AddConstantFillOp({10}, "pred_b");
+                    init_net_->AddXavierFillOp({output_dim_, 500}, "pred_w");
+                    init_net_->AddConstantFillOp({output_dim_}, "pred_b");
                 }
+
+                update_net_->AddSoftmaxOp("pred", "softmax");
                 predict_net_->AddSoftmaxOp("pred", "softmax");
+
+
+
+
                 LOG(INFO)<<predict_model_.external_input().size();
                 for(auto x: predict_model_.external_input()){
                     LOG(INFO)<<x;
@@ -119,16 +168,19 @@ namespace Beta{
             }
 
             void add_loss(){
-                predict_net_->AddLabelCrossEntropyOp("softmax", "label", "xent");
-                predict_net_->AddAveragedLossOp("xent","loss");
-                predict_net_->AddConstantFillWithOp(1.f, "loss", "loss_grad");
-                predict_net_->AddGradientOps();
-                predict_net_->AddLearningRateOp("ITER", "LR", 0.1);
+                update_net_->AddInput("ITER");
+                update_net_->AddLabelCrossEntropyOp("softmax", "pai", "xent");
+                update_net_->AddAveragedLossOp("xent","loss");
+                update_net_->AddConstantFillWithOp(1.f, "loss", "loss_grad");
+                update_net_->AddGradientOps();
+                update_net_->AddLearningRateOp("ITER", "LR", 0.1);
+
 
                 init_net_->AddConstantFillOp({1}, 1.f, "ONE");
-                predict_net_->AddInput("ONE");
-                predict_net_->AddInput("ITER");
-                predict_net_->AddIterOp("ITER");
+                init_net_->AddConstantFillOp({1}, (int64_t)0, "ITER")->mutable_device_option()->set_device_type(CPU);
+                update_net_->AddInput("ONE");
+                
+                update_net_->AddIterOp("ITER");
 
 
                 
@@ -137,22 +189,45 @@ namespace Beta{
                 //}
             }
 
+            void print_shape(){
+                for(auto x: workspace_->Blobs()){
+                    LOG(INFO)<<"workspace: "<<x;
+                    auto y = workspace_->GetBlob(x);
+                    LOG(INFO)<<y->Get<TensorCPU>().dims();
+                }
+            }
+
+            void init_parameters(){
+                init_->Run();
+
+            }
+
+            void update_parameters(){
+
+
+            }
+            void forward(Blob* input, Blob* prob, float& v){
+                Blob* data = workspace_->GetBlob("data");
+                T* d = data->GetMutable<T>();
+                TensorCPU* in = input->GetMutable<TensorCPU>();
+                d->template CopyFrom<CPUContext>(in);
+
+
+
+
+            }
+
 
 
             void train(){
-                init_->Run();
+                
 
 
 
 
                 predict_->Run();
-                for(auto x: workspace_->Blobs()){
-                    LOG(INFO)<<"workspace: "<<x;
-                    auto y = workspace_->GetBlob(x);
-                    LOG(INFO)<<y->Get<TensorCPU>().dims();
-                    LOG(INFO)<<y->Get<TensorCPU>().data<float>()[0];
-                    
-                }
+
+
                 
                 for(int i =0; i< 1; ++ i){
                     predict_->Run();
@@ -176,6 +251,7 @@ namespace Beta{
             void save(string proto_name){
                 WriteProtoToTextFile(predict_model_,proto_name+"_pred.pbtxt");
                 WriteProtoToTextFile(init_model_, proto_name+"_init.pbtxt");
+                WriteProtoToTextFile(update_model_, proto_name+"_update.pbtxt");
             }
 
         protected:
@@ -183,15 +259,23 @@ namespace Beta{
             int batch_size_;
             int channels_;
 
+            int output_dim_;
+
             std::shared_ptr<Net> init_net_;
             std::shared_ptr<Net> predict_net_;
+            std::shared_ptr<Net> update_net_;
+
+
             std::shared_ptr<Workspace> workspace_;
 
             NetDef init_model_;
             NetDef predict_model_;
+            NetDef update_model_;
+
 
             unique_ptr<NetBase> init_;
             unique_ptr<NetBase> predict_;
+            unique_ptr<NetBase> update_;
 
 
     };
--- test/test_network.cc
+++ test/test_network.cc
@@ -6,11 +6,13 @@ using namespace Beta;
 TEST(NETWORK, build){
     NetDef init_model;
     NetDef predict_model;
-    Network<int , int> net(init_model, predict_model);
-    net.init(0);
+    NetDef update_model;
+    Network<int , int, TensorCPU> net(15,1,3);
+
+    net.init(init_model, predict_model,update_model, 0);
     net.create_lenet(1);
     net.add_loss();
-    //net.create();
+    net.allocate();
     net.save("lenet");
     //net.train();
 
