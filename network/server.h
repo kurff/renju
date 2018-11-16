bed02410e5aefbfec229f14bffa617b71b251f23
kurff <kurffu@gmail.com>
2018-11-16 21:56:33 +0800

    update

---
+++ network/server.h
@@ -0,0 +1,21 @@
+#ifndef NETWORK_SERVER_H_
+#define NETWORK_SERVER_H_
+#include "network/network.h"
+namespace Beta{
+    class Server{
+        public:
+            
+
+
+
+    };
+
+
+
+}
+
+
+
+
+
+#endif
